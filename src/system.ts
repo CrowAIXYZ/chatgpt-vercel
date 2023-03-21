a006b6eafdae9df4f7bbdaadad8d2f6a49f3e23b
judezhang <judezhang@tencent.com>
2023-03-21 15:53:13 +0800

    docs: change desc

--- src/system.ts
+++ src/system.ts
@@ -9,7 +9,7 @@ export const setting = {
 
 export const message = `Powered by OpenAI chatGPT and Vercel
 - 由 [@wallezen](https://github.com/wallezen) 基于 [chatgpt-vercel](https://github.com/ourongxing/chatgpt-vercel) 开发部署。
-- 开头输入 [[/]] 或者 [[空格]] 可以展示预设提示词(Prompt)，紧跟在提示词后输入你的问题或提出你的需求。`
+- 开头输入 [[/]] 或者 [[空格]] 可以展示预设提示词(Prompt)，紧跟在提示词后输入你的问题或提出你的需求，可以得到更好的回答。`
 
 export type Setting = typeof setting
 
