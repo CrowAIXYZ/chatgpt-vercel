3c5f443262281004ff06915d0d3c97f1828ebfb0
judezhang <judezhang@tencent.com>
2023-03-21 13:11:59 +0800

    docs: change README file

--- src/system.ts
+++ src/system.ts
@@ -9,7 +9,6 @@ export const setting = {
 
 export const message = `Powered by OpenAI Vercel
 - 由 [@wallezen](https://github.com/wallezen) 基于 [chatgpt-vercel](https://github.com/ourongxing/chatgpt-vercel) 开发，欢迎自部署。
-- 该网站仅作为演示，不提供长期服务，请勿滥用。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
