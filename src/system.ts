63ec7a25eeead78f04cbd5c5e28585e114d7d3cc
judezhang <judezhang@tencent.com>
2023-03-21 15:00:03 +0800

    docs: change desc

--- src/system.ts
+++ src/system.ts
@@ -7,7 +7,7 @@ export const setting = {
   systemRule: ""
 }
 
-export const message = `Powered by OpenAI Vercel
+export const message = `Powered by OpenAI chatGPT and Vercel
 - 由 [@wallezen](https://github.com/wallezen) 基于 [chatgpt-vercel](https://github.com/ourongxing/chatgpt-vercel) 开发部署。
 - 开头输入 [[/]] 或者 [[空格]] 展示预设提示词(Prompt)。`
 
